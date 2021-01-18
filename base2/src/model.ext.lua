365e70ba1f4284c9087d5b6329fd9146287fed69
cryi <cryi@tutanota.com>
2021-01-18 21:48:16 +0100

    update to ami 0.5.0

--- base2/src/model.ext.lua
+++ base2/src/model.ext.lua
@@ -2,4 +2,4 @@ if type(APP.model) ~= "table" then
     APP.model = {}
 end
 
-APP.model = eliUtil.merge_tables(APP.model, { testApp = true })
+APP.model = util.merge_tables(APP.model, { testBase2 = true })
